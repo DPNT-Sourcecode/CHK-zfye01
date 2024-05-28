b3a8ecd936dc3782efcf81c994cd78fd66a5f22c
sbx_user1051 <sbx_user1051@169.254.73.13>
2024-05-28 14:35:40 +0000

    Tue May 28 14:35:40 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -6,8 +6,10 @@ public class CheckoutSolution {
 
 
     public Integer checkout(String skus) {
+        String fixedSkus = skus.toUpperCase();
+
         int[] itemCounts = new int[4]; // count of each item type (A, B, C, D)
-        char[] skuChars = skus.toCharArray(); // characters in skus
+        char[] skuChars = fixedSkus.toCharArray(); // characters in skus
         // Iterate over the skuChars to count to number of each item type
         for (char sku : skuChars) {
             // Switch statement to increment each item type count or invalid type
@@ -29,7 +31,24 @@ public class CheckoutSolution {
             }
         }
 
-        return -1;
+        int runningSum = 0;
+
+        // Handle pricing A
+        runningSum += itemCounts[0] / 3 * 130; // calculate special offers
+        runningSum += itemCounts[0] % 3 * 50; // calculate regular pricing
+
+        // Handle pricing B
+        runningSum += itemCounts[1] / 2 * 45; // calculate special offers
+        runningSum += itemCounts[1] % 2 * 30; // calculate regular pricing
+
+        // Handle pricing C
+        runningSum += itemCounts[2] * 20; // calculate regular pricing
+
+        // Handle pricing D
+        runningSum += itemCounts[3] * 15; // calculate regular pricing
+
+        return runningSum;
     }
 }
+
 
