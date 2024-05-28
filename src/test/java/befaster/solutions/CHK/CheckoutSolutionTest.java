b63010041e9be1f2a595d45144d80d901bc11ff1
sbx_user1051 <sbx_user1051@169.254.73.13>
2024-05-28 14:26:40 +0000

    Tue May 28 14:26:40 UTC 2024

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -13,5 +13,25 @@ public class CheckoutSolutionTest {
     public void setUp() {
         checkout = new CheckoutSolution();
     }
+
+    @Test
+    public void returnsCorrectPricesPerSKU() {
+        assertThat(checkout.checkout("A"), equalTo(50));
+        assertThat(checkout.checkout("B"), equalTo(30));
+        assertThat(checkout.checkout("C"), equalTo(20));
+        assertThat(checkout.checkout("D"), equalTo(15));
+    }
+
+    @Test
+    public void returnsCorrectOfferPricesPerSKU() {
+        assertThat(checkout.checkout("AAA"), equalTo(130));
+        assertThat(checkout.checkout("BB"), equalTo(45));
+    }
+
+    @Test
+    public void returnsCorrectBasketPrice() {
+        assertThat(checkout.checkout("AAABBCD"), equalTo(210));
+    }
     
 }
+
