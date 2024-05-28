d817e27fe9952caa9668e1938f6815b4dc192821
sbx_user1051 <sbx_user1051@169.254.73.13>
2024-05-28 14:23:40 +0000

    Tue May 28 14:23:40 UTC 2024

---
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -0,0 +1,17 @@
+package befaster.solutions.CHK;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class CheckoutSolutionTest {
+    private CheckoutSolution checkout;
+
+    @BeforeEach
+    public void setUp() {
+        checkout = new CheckoutSolution();
+    }
+    
+}
