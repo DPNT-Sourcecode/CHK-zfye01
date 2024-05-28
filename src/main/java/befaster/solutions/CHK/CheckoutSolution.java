8bdc3017e3453144b3547df68bfec7548168c3a8
sbx_user1051 <sbx_user1051@169.254.73.13>
2024-05-28 14:38:40 +0000

    Tue May 28 14:38:40 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -8,27 +8,12 @@ public class CheckoutSolution {
     public Integer checkout(String skus) {
         String fixedSkus = skus.toUpperCase();
 
-        int[] itemCounts = new int[4]; // count of each item type (A, B, C, D)
-        char[] skuChars = fixedSkus.toCharArray(); // characters in skus
-        // Iterate over the skuChars to count to number of each item type
-        for (char sku : skuChars) {
-            // Switch statement to increment each item type count or invalid type
-            switch (sku) {
-                case 'A':
-                    itemCounts[0]++;
-                    break;
-                case 'B':
-                    itemCounts[1]++;
-                    break;
-                case 'C':
-                    itemCounts[2]++;
-                    break;
-                case 'D':
-                    itemCounts[3]++;
-                    break;
-                default: // invalid item type, immediately return
-                    return -1;
-            }
+        int[] itemCounts;
+        
+        try {
+            itemCounts = getItemCounts(fixedSkus);
+        } catch (Exception e) {
+            return -1;
         }
 
         int runningSum = 0;
@@ -49,6 +34,34 @@ public class CheckoutSolution {
 
         return runningSum;
     }
+
+    // Returns an int[] containing the count of different items
+    private int[] getItemCounts(String fixedSkus) throws Exception{
+        int[] itemCounts = new int[4]; // count of each item type (A, B, C, D)
+        char[] skuChars = fixedSkus.toCharArray(); // characters in skus
+        // Iterate over the skuChars to count to number of each item type
+        for (char sku : skuChars) {
+            // Switch statement to increment each item type count or invalid type
+            switch (sku) {
+                case 'A':
+                    itemCounts[0]++;
+                    break;
+                case 'B':
+                    itemCounts[1]++;
+                    break;
+                case 'C':
+                    itemCounts[2]++;
+                    break;
+                case 'D':
+                    itemCounts[3]++;
+                    break;
+                default: // invalid item type, immediately return
+                    throw new Exception("Invalid item type");
+            }
+        }
+        return itemCounts;
+    }
 }
+
 
 
